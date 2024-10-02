461e4eedd1ff4c0a94863da9d6a8e272e7e5ef6f
Samuel Vidal <samuelvsantos2018@gmail.com>
2024-10-01 23:15:39 -0300

    menu pausa

---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    public GameObject pausePanel;
+    private bool isPaused = false;
+
+    private float normalVolume = 0.8f; 
+    private float pausedVolume = 0.4f; 
+
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+
+    public void TogglePause()
+    {
+        Debug.Log("aaaaaaaaaaa");
+        
+        isPaused = !isPaused;
+
+        if (isPaused)
+        {
+            PauseGame();
+        }
+        else
+        {
+            ResumeGame();
+        }
+    }
+
+    void PauseGame()
+    {
+        
+        Time.timeScale = 0f;
+
+        
+        AudioListener.volume = pausedVolume;
+
+        
+        pausePanel.SetActive(true);
+    }
+
+    void ResumeGame()
+    {
+        
+        Time.timeScale = 1f;
+
+        
+        AudioListener.volume = normalVolume;
+
+        
+        pausePanel.SetActive(false);
+    }
+}
